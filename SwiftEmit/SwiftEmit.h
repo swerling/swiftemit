dda8a11bc24318a4731f18e0888776dd9fcc9f97
swerling <sswerling@yahoo.com>
2015-11-11 12:03:38 -0500

    Initial Commit

---
+++ SwiftEmit/SwiftEmit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEmit.h
+//  SwiftEmit
+//
+//  Created by Steven Swerling on 11/11/15.
+//  Copyright © 2015 Steven Swerling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEmit.
+FOUNDATION_EXPORT double SwiftEmitVersionNumber;
+
+//! Project version string for SwiftEmit.
+FOUNDATION_EXPORT const unsigned char SwiftEmitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEmit/PublicHeader.h>
+
+
